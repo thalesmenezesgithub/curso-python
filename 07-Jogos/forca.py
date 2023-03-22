eb29bb74732c19ca87040ada6b55f4c6534eb8a1
Thales R. Menezes <99663601+thalesmenezesgithub@users.noreply.github.com>
2023-03-21 23:19:57 -0300

    Jogo da Forca  - encontrar letra

--- 07-Jogos/forca.py
+++ 07-Jogos/forca.py
@@ -3,6 +3,25 @@ def jogar():
     print("**************************")
     print("Bem-vindo ao Jodo de Forca")
     print("**************************")
-
+    
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou  = False
+    
+    while(not enforcou and not acertou):  
+        chute = input("Qual letra?")
+        chute = chute.strip() #remove os espaços
+        
+        indice = 0
+        for letra in palavra_secreta:
+            
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição {}".format(letra, indice))
+                print(chute)
+                
+            indice = indice + 1
+            
+            
+            
 if(__name__ == "__main__"):
     jogar()
--- 07-Jogos/jogos.py
+++ 07-Jogos/jogos.py
@@ -1,18 +1,22 @@
 import adivinhacao
 import forca
 
-print("****************")
-print("Escolha seu Jogo")
-print("****************")
+def escolha():
+    print("****************")
+    print("Escolha seu Jogo")
+    print("****************")
 
-print("(1) Forca ou (2) Adivinhação")
-jogo = int(input("Qual o Jogo?"))
+    print("(1) Forca ou (2) Adivinhação")
+    jogo = int(input("Qual o Jogo?"))
 
- #chama a função jogar do respectivo arquivo
-if(jogo == 1):
-    print("Jogando Forca!")
-    forca.jogar()
+    #chama a função jogar do respectivo arquivo
+    if(jogo == 1):
+        print("Jogando Forca!")
+        forca.jogar()
 
-elif(jogo == 2):
-    print("Jogando Adivinhação!")
-    adivinhacao.jogar()
+    elif(jogo == 2):
+        print("Jogando Adivinhação!")
+        adivinhacao.jogar()
+        
+if(__name__ == "__main__"):
+    escolha()
