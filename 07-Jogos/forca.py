cda0c4ba14df797e4326bdab5b9300d4b6ea853e
Thales Menezes <99663601+thalesmenezesgithub@users.noreply.github.com>
2023-03-27 14:41:09 -0300

    Palavra implementação "_"

--- 07-Jogos/forca.py
+++ 07-Jogos/forca.py
@@ -4,27 +4,41 @@ def jogar():
     print("Bem-vindo ao Jodo de Forca")
     print("**************************")
     
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "pera".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou  = False
+    erros = 0
     
+    print("")
     print(letras_acertadas)
 
     while(not enforcou and not acertou):  
         chute = input("Qual letra?")
-        chute = chute.strip() #remove os espaços
+        chute = chute.strip().upper() #remove os espaços
         
-        indice = 0
-        for letra in palavra_secreta:
+        if(chute in palavra_secreta):
+
+            indice = 0
+            for letra in palavra_secreta:
             
-            if chute.upper() == letra.upper():
-                letras_acertadas[indice] = letra
-                
-            indice = indice + 1
+                if chute == letra:
+                    letras_acertadas[indice] = letra
+                    
+                indice = indice + 1
         
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-            
-            
+
+    if(acertou):
+        print("Você ganhou!")
+    else:
+            print("Foi enforcado!")
+
+
 if(__name__ == "__main__"):
     jogar()
