9e1fa3e09d24ce53eb6f50e53ce3a8dbf1ff7732
Thales Menezes <99663601+thalesmenezesgithub@users.noreply.github.com>
2023-04-06 14:24:51 -0300

    Melhoria mensagens ao usuário
    Foi melhorado a interface com usuário

--- 07-Jogos/forca.py
+++ 07-Jogos/forca.py
@@ -1,16 +1,58 @@
 import random
 
-def jogar():
-
+def mensagem_abertura():
     print("**************************")
     print("Bem-vindo ao Jodo de Forca")
     print("**************************")
 
-    frutas = ["pera", "uva", "goiaba", "manga","abacate","morango","banana","tomate","abacaxi","acerola","amora","cereja"]
+def gera_palavra_secreta():
     
+    frutas = ["pera", "uva", "goiaba", "manga","abacate","morango","banana","tomate","abacaxi","acerola","amora","cereja","jaca"]
     palavra_secreta = random.choice(frutas).upper()
 
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra_secreta):
+
     letras_acertadas = ["_" for letra in palavra_secreta]
+
+    return letras_acertadas
+
+def imprime_mensagem(acertou, palavra_secreta):
+    if(acertou):
+        print("Parabéns, você ganhou!")
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")
+    else:
+        print("Puxa, você foi enforcado!")
+        print("A palavra era {}".format(palavra_secreta))
+        print("    _______________         ")
+        print("   /               \       ")
+        print("  /                 \      ")
+        print("//                   \/\  ")
+        print("\|   XXXX     XXXX   | /   ")
+        print(" |   XXXX     XXXX   |/     ")
+        print(" |   XXX       XXX   |      ")
+        print(" |                   |      ")
+        print(" \__      XXX      __/     ")
+        print("   |\     XXX     /|       ")
+        print("   | |           | |        ")
+        print("   | I I I I I I I |        ")
+        print("   |  I I I I I I  |        ")
+        print("   \_             _/       ")
+        print("     \_         _/         ")
+        print("       \_______/           ")
+
+def verifica_acerto_palavra(letras_acertadas, palavra_secreta):
+
     enforcou = False
     acertou  = False
     erros = 0
@@ -39,10 +81,18 @@ def jogar():
         acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
-    if(acertou):
-        print("Você ganhou!")
-    else:
-            print("Foi enforcado!")
+    imprime_mensagem(acertou, palavra_secreta)
+
+
+def jogar():
+
+    mensagem_abertura()
+
+    palavra_secreta = gera_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    verifica_acerto_palavra(letras_acertadas, palavra_secreta)
 
 
 if(__name__ == "__main__"):
